1e1c7c77b51c3e7853313e3b8a1a3c034da336b8
dram1008 <dram1008@yandex.ru>
2015-08-18 13:44:54 +0300

    stock prognoz add

--- service/GraphExporter.php
+++ service/GraphExporter.php
@@ -161,8 +161,8 @@ class GraphExporter extends Object
         $max = null;
         foreach ($this->rows as $row) {
             $dateArray = ArrayHelper::getColumn($row, 'date');
-            VarDumper::dump($dateArray);
             $dateArray = sort($dateArray);
+            VarDumper::dump($dateArray);
             $dateArray = array_reverse($dateArray);
             if (is_null($max)) {
                 $max = $dateArray[0];
