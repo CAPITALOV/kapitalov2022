c14f86dcd78249377d78414ea51ab6b6bc082d7d
dram1008 <dram1008@yandex.ru>
2015-08-20 18:45:29 +0300

    Подключить платежную систему #91

--- controllers/Yandex_moneyController.php
+++ controllers/Yandex_moneyController.php
@@ -49,22 +49,8 @@ class Yandex_moneyController extends BaseController
         /** @var \app\service\authclient\YandexMoney $client */
         $client = Yii::$app->authClientCollection->getClient('yandex_money');
         $auth_url = API::buildObtainTokenUrl($client->clientId, 'http://c.galaxysss.ru/yandexMoney', ['account-info']);
-//        Yii::$app->response->redirect($auth_url);
         $client->auth22();
     }
-//    public function actions()
-//    {
-//        return [
-//            'auth' => [
-//                'class'           => 'app\service\authclient\AuthAction',
-//                'successCallback' => [
-//                    $this,
-//                    'successCallback'
-//                ],
-//            ],
-//        ];
-//
-//    }
 
     /**
      * @param \yii\authclient\ClientInterface $client
