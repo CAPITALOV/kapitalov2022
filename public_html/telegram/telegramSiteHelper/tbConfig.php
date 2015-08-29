23f815ab29a6d1baa433b0e307501c2ed11429e4
dram1008 <dram1008@yandex.ru>
2015-08-29 21:22:05 +0300

    Добавить чат #174

--- public_html/telegram/telegramSiteHelper/tbConfig.php
+++ public_html/telegram/telegramSiteHelper/tbConfig.php
@@ -7,11 +7,11 @@ $tbRootDir = $_SERVER['DOCUMENT_ROOT'] . "/telegram/telegramSiteHelper";
 
 // Пароль для авторизации менеджера через телеграм бота
 // Password for manager auth (in Telegram bot), you must write some non-so-easy
-$tbManagerPassword = "123456";
+$tbManagerPassword = "123";
 
 // API Token, который вы получили у @BotFather
 // API Token, you can get it from user @BotFather (in Telegram App)
-$tbAPIToken = "";
+$tbAPIToken = "114576187:AAEviNK7YoWjuH5zVgW50Kc8Jg4FrXiLtnA";
 
 
 // Тип базы данных: «sqlite» или «mysql»
@@ -22,9 +22,9 @@ $dbUse = "mysql";
 // Если вы используете MySQL - укажите ниже хост, имя базы, логин и пароль
 // If you use MySQL, write here host, dbname, login, password
 $mysqlHost = "localhost";
-$mysqlDB = "galaxysss_1";
-$mysqlLogin = "galaxysss_1";
-$mysqlPassword = "dram1008";
+$mysqlDB = "galaxysss_4";
+$mysqlLogin = "galaxysss_41";
+$mysqlPassword = "galaxysss_41";
 
 
 #######################################################################
