26b10fa01d5e2921094da97b49797ea7e55594bd
dram1008 <dram1008@yandex.ru>
2015-08-31 20:50:43 +0300

    График будущего не работает, поправить #212

--- views/cabinet/stock_item3.php
+++ views/cabinet/stock_item3.php
@@ -55,7 +55,7 @@ $colorBlue = [
     if ($logo) {
         ?>
         <div class="col-lg-3">
-            <img src="<?= $logo ?>" class="thumbnail">
+            <img src="<?= $logo ?>" class="thumbnail" width="50">
         </div>
     <?php
     }
@@ -74,110 +74,119 @@ $colorBlue = [
 </div>
 
 
-<h2 class="page-header">Прошлое</h2>
+<!--<h2 class="page-header">Прошлое</h2>-->
 <?php
-$graph3 = new \cs\Widget\ChartJs\Line([
-    'width'     => 800,
-    'lineArray' => $lineArrayPast,
-    'colors'    => [
-        $colorGreen,
-        $colorRed,
-        $colorBlue,
-    ],
-]);
-echo $graph3->run();
-$url = Url::to(['cabinet/graph_ajax']);
+//$graph3 = new \cs\Widget\ChartJs\Line([
+//    'width'     => 800,
+//    'lineArray' => $lineArrayPast,
+//    'colors'    => [
+//        $colorGreen,
+//        $colorRed,
+//        $colorBlue,
+//    ],
+//]);
+//echo $graph3->run();
+//$url = Url::to(['cabinet/graph_ajax']);
+//
+//$timeEnd = time() - 60 * 60 * 24;
+//$timeStart = $timeEnd - 60 * 60 * 24 * 30 * 6;
+//$defaultEnd = $timeEnd;
+//$defaultStart = $defaultEnd - 60 * 60 * 24 * 30;
+//
+//$this->registerJs(<<<JS
+//    /**
+//    *
+//    * @param f float
+//    */
+//    function getDate(f)
+//    {
+//        start = new Date();
+//        start.setTime(parseInt(f) + '000');
+//        var m = start.getMonth() + 1;
+//        if (m < 10) m = '0' + m;
+//        var d = start.getDate();
+//        if (d < 10) d = '0' + d;
+//
+//        return start.getFullYear() + '-' + m + '-' + d;
+//    }
+//
+//    $('#slider').rangeSlider({
+//        bounds: {min: {$timeStart}, max: {$timeEnd}},
+//        formatter: function(val) {
+//            var d = new Date();
+//            d.setTime(parseInt(val) + '000');
+//            var out = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
+//
+//            return out;
+//        },
+//        defaultValues:{min: {$defaultStart}, max: {$defaultEnd}}
+//    });
+//    var functionOnChange = function(e, data)  {
+//        {$graph3->varName}.destroy();
+//        var values = $('#slider').rangeSlider('values');
+//        var start = getDate(values.min);
+//        var end = getDate(values.max);
+//        console.log([start, end ]);
+//        ajaxJson({
+//            url: '$url',
+//            data: {
+//                'min': start,
+//                'max': end,
+//                'id': {$item->getId()},
+//                'isUseRed': $('#stockitem3-isred').is(':checked')? 1 : 0,
+//                'isUseBlue': $('#stockitem3-isblue').is(':checked')? 1 : 0,
+//                'isUseKurs': $('#stockitem3-iskurs').is(':checked')? 1 : 0,
+//                'y': 1
+//            },
+//            success: function(ret) {
+//                {$graph3->varName} = new Chart(document.getElementById('$graph3->id').getContext('2d')).Line(ret, []);
+//            }
+//        });
+//    };
+//    $("#slider").bind("valuesChanged", functionOnChange);
+//    $('#stockitem3-isred').change(functionOnChange);
+//    $('#stockitem3-isblue').change(functionOnChange);
+//    $('#stockitem3-iskurs').change(functionOnChange);
+//JS
+//);
+//?>
+<!--<div class="row col-lg-12">-->
+<!--    <div class="row col-lg-8">-->
+<!--        <div style="margin: 10px 0px 20px 0px;width: 800px;">-->
+<!--            <div id="slider" style=""></div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!--<div class="row col-lg-12">-->
+<!--    --><?php
+//    $model = new \app\models\Form\StockItem3();
+//    $form = ActiveForm::begin([
+//        'id' => 'contact-form2',
+//    ]);
+//    ?>
+<!--    <div class="col-lg-1">-->
+<!--        Прогноз-->
+<!--        --><?//= $form->field($model, 'isRed')->widget('cs\Widget\CheckBox2\CheckBox', ['options' => ['data-onstyle' => 'danger']])->label('', ['class' => 'hide'])?>
+<!--    </div>-->
+<!--    <div class="col-lg-1">-->
+<!--        Прогноз-->
+<!--        --><?//= $form->field($model, 'isBlue')->widget('cs\Widget\CheckBox2\CheckBox', ['options' => ['data-onstyle' => 'primary']])->label('', ['class' => 'hide']) ?>
+<!--    </div>-->
+<!--    <div class="col-lg-1">-->
+<!--        Курс-->
+<!--        --><?//= $form->field($model, 'isKurs')->widget('cs\Widget\CheckBox2\CheckBox', ['options' => ['data-onstyle' => 'success']])->label('', ['class' => 'hide']) ?>
+<!--    </div>-->
+<!--    --><?php //ActiveForm::end() ?>
+<!--</div>-->
 
-$timeEnd = time() - 60 * 60 * 24;
-$timeStart = $timeEnd - 60 * 60 * 24 * 30 * 6;
-$defaultEnd = $timeEnd;
-$defaultStart = $defaultEnd - 60 * 60 * 24 * 30;
 
-$this->registerJs(<<<JS
-    /**
-    *
-    * @param f float
-    */
-    function getDate(f)
-    {
-        start = new Date();
-        start.setTime(parseInt(f) + '000');
-        var m = start.getMonth() + 1;
-        if (m < 10) m = '0' + m;
-        var d = start.getDate();
-        if (d < 10) d = '0' + d;
 
-        return start.getFullYear() + '-' + m + '-' + d;
-    }
 
-    $('#slider').rangeSlider({
-        bounds: {min: {$timeStart}, max: {$timeEnd}},
-        formatter: function(val) {
-            var d = new Date();
-            d.setTime(parseInt(val) + '000');
-            var out = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
 
-            return out;
-        },
-        defaultValues:{min: {$defaultStart}, max: {$defaultEnd}}
-    });
-    var functionOnChange = function(e, data)  {
-        {$graph3->varName}.destroy();
-        var values = $('#slider').rangeSlider('values');
-        var start = getDate(values.min);
-        var end = getDate(values.max);
-        console.log([start, end ]);
-        ajaxJson({
-            url: '$url',
-            data: {
-                'min': start,
-                'max': end,
-                'id': {$item->getId()},
-                'isUseRed': $('#stockitem3-isred').is(':checked')? 1 : 0,
-                'isUseBlue': $('#stockitem3-isblue').is(':checked')? 1 : 0,
-                'isUseKurs': $('#stockitem3-iskurs').is(':checked')? 1 : 0,
-                'y': 1
-            },
-            success: function(ret) {
-                {$graph3->varName} = new Chart(document.getElementById('$graph3->id').getContext('2d')).Line(ret, []);
-            }
-        });
-    };
-    $("#slider").bind("valuesChanged", functionOnChange);
-    $('#stockitem3-isred').change(functionOnChange);
-    $('#stockitem3-isblue').change(functionOnChange);
-    $('#stockitem3-iskurs').change(functionOnChange);
-JS
-);
-?>
-<div class="row col-lg-12">
-    <div class="row col-lg-8">
-        <div style="margin: 10px 0px 20px 0px;width: 800px;">
-            <div id="slider" style=""></div>
-        </div>
-    </div>
-</div>
-<div class="row col-lg-12">
-    <?php
-    $model = new \app\models\Form\StockItem3();
-    $form = ActiveForm::begin([
-        'id' => 'contact-form',
-    ]);
-    ?>
-    <div class="col-lg-1">
-        Прогноз
-        <?= $form->field($model, 'isRed')->widget('cs\Widget\CheckBox2\CheckBox', ['options' => ['data-onstyle' => 'danger']])->label('', ['class' => 'hide'])?>
-    </div>
-    <div class="col-lg-1">
-        Прогноз
-        <?= $form->field($model, 'isBlue')->widget('cs\Widget\CheckBox2\CheckBox', ['options' => ['data-onstyle' => 'primary']])->label('', ['class' => 'hide']) ?>
-    </div>
-    <div class="col-lg-1">
-        Курс
-        <?= $form->field($model, 'isKurs')->widget('cs\Widget\CheckBox2\CheckBox', ['options' => ['data-onstyle' => 'success']])->label('', ['class' => 'hide']) ?>
-    </div>
-    <?php ActiveForm::end() ?>
-</div>
+
+
+
+
 <h2 class="page-header row col-lg-12" style="page-break-before: always;">Будущее</h2>
 <?php if ($isPaid) { ?>
     <?php
