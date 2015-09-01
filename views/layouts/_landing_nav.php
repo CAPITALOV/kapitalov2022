e1e964a5c5ceb68e84ff9dbf22e2acd75622d55c
dram1008 <dram1008@yandex.ru>
2015-09-01 13:33:28 +0300

    Модификация лендинга #224

--- views/layouts/_landing_nav.php
+++ views/layouts/_landing_nav.php
@@ -18,6 +18,11 @@ use yii\helpers\Html;
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="/">Capitalov.com</a>
+            <ul class="nav navbar-nav navbar-left">
+                <li>
+                    <a href="javascript:void(0)">+7-(499)-394-27-43</a>
+                </li>
+                </ul>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
