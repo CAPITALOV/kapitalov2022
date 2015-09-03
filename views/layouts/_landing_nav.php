4981fab4800a92904292369858bcf54c13478204
dram1008 <dram1008@yandex.ru>
2015-09-04 02:40:26 +0300

    Поправить NavBar #254

--- views/layouts/_landing_nav.php
+++ views/layouts/_landing_nav.php
@@ -11,13 +11,9 @@ use yii\helpers\Html;
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
+            <a class="navbar-brand" href="/" style="padding: 5px 10px 5px 10px;"><img
+                    src="<?= Yii::$app->getAssetManager()->getBundle('app\assets\LayoutSite\Asset')->baseUrl ?>/images/capitalovlogo2.png"
+                    class="siteLayoutLogo"></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
