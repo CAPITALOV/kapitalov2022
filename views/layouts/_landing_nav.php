9d83d7d1e40d5130dc1b00e7281a15017365ffd7
dram1008 <dram1008@yandex.ru>
2015-09-04 02:39:39 +0300

    Поправить NavBar #254

--- views/layouts/_landing_nav.php
+++ views/layouts/_landing_nav.php
@@ -7,66 +7,56 @@ use yii\helpers\Html;
 
 ?>
 <!-- Navigation -->
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <a class="navbar-brand" href="/" style="padding: 5px 10px 5px 10px;">1</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
 
+        <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-
+                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
             </ul>
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
             <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="<?= Url::to(['site/about']) ?>">О продукте</a>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
                 </li>
-                <li>
-                    <a href="<?= Url::to(['site/price']) ?>">Тарифы</a>
-                </li>
-
-                <?php if (Yii::$app->user->isGuest) { ?>
-                    <li>
-                        <a href="<?= Url::to(['auth/registration']) ?>">Регистрация</a>
-                    </li>
-                    <li>
-                        <a href="<?= Url::to(['auth/login']) ?>" class="linkEnter" style="color: #008000">Личный кабинет</a>
-                    </li>
-                <?php } else { ?>
-                    <li>
-                        <!-- Split button -->
-                        <div class="btn-group" style="margin-top: 9px;" id="loginBarButton">
-                            <a href="<?= Url::to(['cabinet/index']) ?>" class="btn btn-default" id="modalLogin"><i
-                                    class="glyphicon glyphicon-user" style="padding-right: 5px;"></i>Кабинет</a>
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"
-                                    aria-expanded="false">
-                                <span class="caret"></span>
-                                <span class="sr-only">Toggle Dropdown</span>
-                            </button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="<?= Url::to(['cabinet/profile']) ?>"><i class="glyphicon glyphicon-cog"
-                                                                                     style="padding-right: 5px;"></i>Мой
-                                        профиль</a></li>
-                                <li><a href="<?= Url::to(['cabinet/password_change']) ?>"><i
-                                            class="glyphicon glyphicon-asterisk" style="padding-right: 5px;"></i>Сменить
-                                        пароль</a></li>
-                                <li class="divider"></li>
-                                <li><a href="<?= Url::to(['money/history']) ?>"><i
-                                            class="glyphicon glyphicon-rub" style="padding-right: 5px;"></i>История
-                                        платежей</a></li>
-                                <li class="divider"></li>
-                                <li><a href="<?= Url::to(['auth/logout']) ?>" data-method="post"><i
-                                            class="glyphicon glyphicon-off" style="padding-right: 5px;"></i>Выйти</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                <?php } ?>
             </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
---
+++ views/layouts/_landing_nav1.php
@@ -0,0 +1,74 @@
+<?php
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+/** @var  $this \yii\web\View */
+
+?>
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/" style="padding: 5px 10px 5px 10px;"><img
+                    src="<?= Yii::$app->getAssetManager()->getBundle('app\assets\LayoutSite\Asset')->baseUrl ?>/images/capitalovlogo2.png"
+                    class="siteLayoutLogo"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="<?= Url::to(['site/about']) ?>">О продукте</a>
+                </li>
+                <li>
+                    <a href="<?= Url::to(['site/price']) ?>">Тарифы</a>
+                </li>
+
+                <?php if (Yii::$app->user->isGuest) { ?>
+                    <li>
+                        <a href="<?= Url::to(['auth/registration']) ?>">Регистрация</a>
+                    </li>
+                    <li>
+                        <a href="<?= Url::to(['auth/login']) ?>" class="linkEnter" style="color: #008000">Личный кабинет</a>
+                    </li>
+                <?php } else { ?>
+                    <li>
+                        <!-- Split button -->
+                        <div class="btn-group" style="margin-top: 9px;" id="loginBarButton">
+                            <a href="<?= Url::to(['cabinet/index']) ?>" class="btn btn-default" id="modalLogin"><i
+                                    class="glyphicon glyphicon-user" style="padding-right: 5px;"></i>Кабинет</a>
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"
+                                    aria-expanded="false">
+                                <span class="caret"></span>
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="<?= Url::to(['cabinet/profile']) ?>"><i class="glyphicon glyphicon-cog"
+                                                                                     style="padding-right: 5px;"></i>Мой
+                                        профиль</a></li>
+                                <li><a href="<?= Url::to(['cabinet/password_change']) ?>"><i
+                                            class="glyphicon glyphicon-asterisk" style="padding-right: 5px;"></i>Сменить
+                                        пароль</a></li>
+                                <li class="divider"></li>
+                                <li><a href="<?= Url::to(['money/history']) ?>"><i
+                                            class="glyphicon glyphicon-rub" style="padding-right: 5px;"></i>История
+                                        платежей</a></li>
+                                <li class="divider"></li>
+                                <li><a href="<?= Url::to(['auth/logout']) ?>" data-method="post"><i
+                                            class="glyphicon glyphicon-off" style="padding-right: 5px;"></i>Выйти</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
