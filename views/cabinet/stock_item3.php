b7eed1846991d6f734a181f6542d155ff3eb85ff
dram1008 <dram1008@yandex.ru>
2015-09-06 14:09:30 +0300

    Заменить #279

--- views/cabinet/stock_item3.php
+++ views/cabinet/stock_item3.php
@@ -69,7 +69,85 @@ if ($d) {
 ?>
 
 
-<h2 class="page-header">Прошлое</h2>
+
+<h2 class="page-header row col-lg-12" style="page-break-before: always;">Индексы капиталов</h2>
+<?php if ($isPaid) { ?>
+    <?php
+
+    $graphFuture = new \cs\Widget\ChartJs\Line([
+        'width'     => 800,
+        'lineArray' => $lineArrayFuture,
+        'colors'    => [
+            $colorRed,
+            $colorBlue,
+        ],
+    ]);
+    echo $graphFuture->run();
+
+    $timeStart = time();
+    $timeEnd = $timeStart + 60 * 60 * 24 * 30;
+    $defaultEnd = $timeStart;
+    $defaultStart = $timeEnd;
+
+    $this->registerJs(<<<JS
+    $('#sliderFuture').rangeSlider({
+        bounds: {min: {$timeStart}, max: {$timeEnd}},
+        formatter: function(val) {
+            var d = new Date();
+            d.setTime(parseInt(val) + '000');
+            var out = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
+
+            return out;
+        },
+        defaultValues:{min: {$defaultStart}, max: {$defaultEnd}}
+    });
+    var functionOnChangeFuture = function(e, data) {
+        {$graphFuture->varName}.destroy();
+        var start = getDate(data.values.min);
+        var end = getDate(data.values.max);
+        ajaxJson({
+            url: '$url',
+            data: {
+                'min': start,
+                'max': end,
+                'id': {$item->getId()},
+                'isUseRed': 1,
+                'isUseBlue': 1,
+                'isUseKurs': 0,
+                'y': 2
+            },
+            success: function(ret) {
+                    {$graphFuture->varName} = new Chart(document.getElementById('$graphFuture->id').getContext('2d')).Line(ret, []);
+            }
+        });
+    };
+    $("#sliderFuture").bind("valuesChanged", functionOnChangeFuture);
+
+JS
+    );
+    ?>
+
+    <div class="row col-lg-12">
+        <div class="row col-lg-8">
+            <div style="margin: 10px 0px 20px 0px; width: 800px;">
+                <div id="sliderFuture"></div>
+            </div>
+        </div>
+    </div>
+<?php } else { ?>
+    <div class="form-group">
+        <p><span class="label label-danger">График не оплачен</span></p>
+    </div>
+    <a
+        href="<?= Url::to(['cabinet_wallet/add', 'id' => $item->getId()]) ?>"
+        class="btn btn-default"
+        style="width: 100%"
+        >Купить</a>
+<?php } ?>
+
+
+
+<h2 class="page-header">Предыстория торгов</h2>
 
 <?php
 $model = new \app\models\Form\StockItem3();
@@ -200,81 +278,6 @@ JS
 
 
 
-
-<h2 class="page-header row col-lg-12" style="page-break-before: always;">Будущее</h2>
-<?php if ($isPaid) { ?>
-    <?php
-
-    $graphFuture = new \cs\Widget\ChartJs\Line([
-        'width'     => 800,
-        'lineArray' => $lineArrayFuture,
-        'colors'    => [
-            $colorRed,
-            $colorBlue,
-        ],
-    ]);
-    echo $graphFuture->run();
-
-    $timeStart = time();
-    $timeEnd = $timeStart + 60 * 60 * 24 * 30;
-    $defaultEnd = $timeStart;
-    $defaultStart = $timeEnd;
-
-    $this->registerJs(<<<JS
-    $('#sliderFuture').rangeSlider({
-        bounds: {min: {$timeStart}, max: {$timeEnd}},
-        formatter: function(val) {
-            var d = new Date();
-            d.setTime(parseInt(val) + '000');
-            var out = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
-
-            return out;
-        },
-        defaultValues:{min: {$defaultStart}, max: {$defaultEnd}}
-    });
-    var functionOnChangeFuture = function(e, data) {
-        {$graphFuture->varName}.destroy();
-        var start = getDate(data.values.min);
-        var end = getDate(data.values.max);
-        ajaxJson({
-            url: '$url',
-            data: {
-                'min': start,
-                'max': end,
-                'id': {$item->getId()},
-                'isUseRed': 1,
-                'isUseBlue': 1,
-                'isUseKurs': 0,
-                'y': 2
-            },
-            success: function(ret) {
-                    {$graphFuture->varName} = new Chart(document.getElementById('$graphFuture->id').getContext('2d')).Line(ret, []);
-            }
-        });
-    };
-    $("#sliderFuture").bind("valuesChanged", functionOnChangeFuture);
-
-JS
-    );
-    ?>
-
-    <div class="row col-lg-12">
-        <div class="row col-lg-8">
-            <div style="margin: 10px 0px 20px 0px; width: 800px;">
-                <div id="sliderFuture"></div>
-            </div>
-        </div>
-    </div>
-<?php } else { ?>
-    <div class="form-group">
-        <p><span class="label label-danger">График не оплачен</span></p>
-    </div>
-    <a
-        href="<?= Url::to(['cabinet_wallet/add', 'id' => $item->getId()]) ?>"
-        class="btn btn-default"
-        style="width: 100%"
-        >Купить</a>
-<?php } ?>
 
 <div class="modal fade" id="myModalBlue" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
