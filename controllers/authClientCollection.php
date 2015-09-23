d6aa76494f9ef697c51de739116b89eea8d30afb
dram1008 <dram1008@yandex.ru>
2015-09-24 00:03:42 +0300

    Сделать регистрацию через фейсбук #311

--- controllers/authClientCollection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'facebook'  => [
-        'class'        => 'app\service\authclient\Facebook',
-        'clientId'     => '698681546931007',
-        'clientSecret' => 'dd8a083ab3a08e7031fe2de818985f08',
-    ],
-];
