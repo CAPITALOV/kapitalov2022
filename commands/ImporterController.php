3623c32f78470553d6a02f1fe23183536094d2a1
Святослав Архангельский <god@galaxysss.ru>
2016-10-29 15:52:20 +0300

    1

--- commands/ImporterController.php
+++ commands/ImporterController.php
@@ -82,7 +82,6 @@ class ImporterController extends Controller
                 ['not', ['finam_market' => '']]
             ])
             ->all();
-        \cs\services\VarDumper::dump($rows, true);
 
         foreach($rows as $row) {
             $this->log('Try to get: ' . $row['name']);
@@ -90,6 +89,7 @@ class ImporterController extends Controller
             $start = (new \DateTime())->sub(new \DateInterval('P7D'));
             $end = (new \DateTime());
             $result = \app\models\Form\StockKursImport::importCandels($row['id'], $start->format('Y-m-d'), $end->format('Y-m-d'));
+            \cs\services\VarDumper::dump($result, true);
             $new = $result['insert'];
             if (count($new) > 0) {
                 \Yii::info('Импортированы данные: ' . VarDumper::dumpAsString($new), 'cap\\importer\\index');
