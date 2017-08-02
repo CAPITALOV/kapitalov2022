2bfd5db2aa9dc6bf876a2bf61f190628fb7ef1e7
ArhangelSky SvyatoSlav <dram1008@yandex.ru>
2017-08-03 00:57:58 +0300

    1

--- views/layouts/site.php
+++ views/layouts/site.php
@@ -56,7 +56,7 @@ $this->registerMetaTag(['name' => 'og:description', 'content' => 'Эксклюз
     <footer>
         <div class="row">
             <div class="col-lg-12">
-                <p>© 2002 — <?= date('Y') ?> | www.capitalov.com | Национальное Агентство Капиталов | <?= $this->render('../blocks/telephone') ?></p>
+                <p>© 2002 — <?= date('Y') ?> | www.kapitalov.com | Национальное Агентство Капиталов | <?= $this->render('../blocks/telephone') ?></p>
             </div>
         </div>
     </footer>
