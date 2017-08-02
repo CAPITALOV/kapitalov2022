9f251bae91cca576aed4765cbdd95d216b28401c
ArhangelSky SvyatoSlav <dram1008@yandex.ru>
2017-08-03 00:24:44 +0300

    1

--- views/site/price.php
+++ views/site/price.php
@@ -76,42 +76,5 @@ $this->registerJs("$('.payImage').tooltip()");
             </ul>
         </div>
     </div>
-    <h2 class="page-header col-lg-12">Мы принимаем к оплате</h2>
-
-    <div class="col-md-1">
-        <img src="/images/site/price/yandex_money.jpg" width="100%" class="thumbnail payImage" alt="Яндекс Деньги"
-             title="Яндекс Деньги">
-    </div>
-    <div class="col-md-1">
-        <img src="/images/site/price/mastercard.png" width="100%" class="thumbnail payImage"
-             alt="Банковские карты Master Card" title="Банковские карты Master Card">
-    </div>
-    <div class="col-md-1">
-        <img src="/images/site/price/visa.png" width="100%" class="thumbnail payImage" alt="Банковские карты Visa"
-             title="Банковские карты Visa">
-    </div>
-    <div class="col-md-1">
-        <img src="/images/site/price/terminal.jpg" width="100%" class="thumbnail payImage" alt="Платежи через термиалы"
-             title="Платежи через термиалы">
-    </div>
-    <div class="col-md-1">
-        <img src="/images/site/price/webmoney.jpg" width="100%" class="thumbnail payImage" alt="WebMoney"
-             title="WebMoney">
-    </div>
-    <div class="col-md-1">
-        <img src="/images/site/price/masterpass.png" width="100%" class="thumbnail payImage" alt="MasterPass"
-             title="MasterPass">
-    </div>
-    <div class="col-md-1">
-        <img src="/images/site/price/alfa-click.jpg" width="100%" class="thumbnail payImage" alt="Альфа-Клик"
-             title="Альфа-Клик">
-    </div>
-    <div class="col-md-1">
-        <img src="/images/site/price/psb.jpg" width="100%" class="thumbnail payImage" alt="Промсвязьбанк"
-             title="Промсвязьбанк">
-    </div>
-
-
-
 </div>
 <!-- /.row -->
