3921fd8c680aa1a33f6fd74c06fde00417c79e4d
ArhangelSky SvyatoSlav <dram1008@yandex.ru>
2017-09-23 20:17:08 +0300

    1

--- views/blocks/telephone.php
+++ views/blocks/telephone.php
@@ -1,3 +1,11 @@
-<a href="tel:+79998980442">
-    +7 (999) 898-0442
+<?php
+
+$t = \Yii::$app->params['tel'];
+
+$telephone2 = substr($t,0,2) . ' (' . substr($t,2,3) . ') ' . substr($t,5,3) . '-' . substr($t,7,4);
+
+?>
+
+<a href="tel:<?= $t ?>">
+    <?= $telephone2 ?>
 </a>
