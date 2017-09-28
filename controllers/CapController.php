569111a43ef7bd7e37f54783e35820c6f195dddd
ArhangelSky SvyatoSlav <dram1008@yandex.ru>
2017-09-28 21:56:28 +0300

    1

---
+++ controllers/CapController.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Класс для действий незерегистрированного пользователя
+ *
+ */
+
+namespace app\controllers;
+
+use app\models\Log;
+use app\models\Stock;
+use app\models\StockKurs;
+use app\models\User;
+use app\service\LogReader;
+use cs\services\Security;
+use cs\services\Url;
+use Yii;
+use yii\db\Query;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+use yii\helpers\FileHelper;
+use yii\helpers\Json;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\helpers\VarDumper;
+use yii\data\ActiveDataProvider;
+use app\models\LoginForm;
+use app\models\ContactForm;
+use app\models\UserForm;
+use app\models\Form\UserPassword as FormUserPassword;
+
+class SiteController extends \cs\base\BaseController
+{
+    public $layout = 'blank';
+
+    public function actionCap()
+    {
+        return $this->render('cap');
+    }
+}
---
+++ views/layouts/blank.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: god
+ * Date: 28.09.2017
+ * Time: 21:55
+ */
+/** @var $this \yii\web\View */
+
+?>
+<?= $content ?>
