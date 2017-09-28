e50e90a824fbb9e48461bf50049e0ee5004bdb88
ArhangelSky SvyatoSlav <dram1008@yandex.ru>
2017-09-28 21:58:44 +0300

    1

--- config/urlRules.php
+++ config/urlRules.php
@@ -12,7 +12,7 @@ return [
     'log'                                   => 'site/log',
     'logDb'                                 => 'site/log_db',
     'referal'                               => 'site/referal',
-    'cap'                                   => 'cap/referal',
+    'cap'                                   => 'cap/index',
 
     'about'                                 => 'site/about',
 
