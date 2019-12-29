f1ff82f59aad5341d0d927c48a55cd073897d679
Ra-m-ha <dram1008@yandex.ru>
2019-12-29 21:34:55 +0300

    1

--- config/mailerTransport.php
+++ config/mailerTransport.php
@@ -5,15 +5,11 @@ return [
     'useFileTransport' => false,
     'transport' => [
         'class'    => 'Swift_SmtpTransport',
-//        'host'     => 'smtp.timeweb.ru',
-//        'port'     => '25',
-//        'username' => 'info@s-routes.com',
-//        'password' => 'f6Fx66CX',
 
         'username' => 'kapitalov2020@ya.ru',
         'password' => 'mtS-WVL-Qzr-3fW',
         'host'     => 'smtp.yandex.ru',
-        'port'     => 465,
+        'port'     => '465',
         'encryption' => 'SSL',
     ]
 ];
