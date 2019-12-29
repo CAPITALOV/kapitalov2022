d6b8b2d64a05ed23c4c4305e27f044790e6deba3
Ra-m-ha <dram1008@yandex.ru>
2019-12-29 21:25:58 +0300

    1

---
+++ config/1.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'class'            => 'yii\swiftmailer\Mailer',
+    'useFileTransport' => false,
+    'transport' => [
+        'class'    => 'Swift_SmtpTransport',
+        'host'     => 'smtp.timeweb.ru',
+        'port'     => '25',
+        'username' => 'info@s-routes.com',
+        'password' => 'f6Fx66CX',
+
+//        'username' => 'kapitalov2020@ya.ru',
+//        'password' => 'mtS-WVL-Qzr-3fW',
+//        'host'     => 'smtp.yandex.ru',
+//        'port'     => 25,
+    ]
+];
