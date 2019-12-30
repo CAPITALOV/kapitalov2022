12753248e6582c21411d9cd98a51236978d0d4be
Ra-m-ha <dram1008@yandex.ru>
2019-12-30 18:59:36 +0300

    1

--- views/blocks/telephone.php
+++ views/blocks/telephone.php
@@ -2,7 +2,7 @@
 
 $t = \Yii::$app->params['tel'];
 
-$telephone2 = substr($t,0,2) . ' (' . substr($t,2,3) . ') ' . substr($t,5,3) . '-' . substr($t,7,4);
+$telephone2 = substr($t,0,2) . ' (' . substr($t,2,3) . ') ' . substr($t,5,3) . '-' . substr($t,8,4);
 
 ?>
 
